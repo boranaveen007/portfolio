fe916aa60cf6ba83da73b67e3b0fe99e125a07e0
naveen bora <boranaveen33@gmail.com>
2025-03-20 00:10:59 -0400

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,8 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Enables static export
-  images: {
-    unoptimized: true, // Required for Next.js static export
-  },
-  basePath: "/portfolio", // Replace with your GitHub repo name
-  assetPrefix: "/portfolio/", // Replace with your GitHub repo name
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
